84f117773d87284d913dfe29a0835b4d68d957c2
taxcareldj <taxcuanldj@gmail.com>
2025-11-08 18:16:46 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         {/* ===== Logo Area ===== */}
         <a href="#" className="flex items-center gap-2">
           <img
-            src="/KilauJaktimnonbg.png" // pastikan file ini ada di folder public/
+            src="/KilauJaktimwhite.png" // pastikan file ini ada di folder public/
             alt="Kilau Jaktim Logo"
             className="h-8 w-auto"
           />
